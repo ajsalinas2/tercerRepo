180ee400c2529ea49c268fbe277b2a5fc080b673
ajsalinas2 <alvarojsalinas@gmail.com>
2025-07-25 17:35:34 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Alvaro Salinas",                         # Tu nombre
+    author_email="alvarojsalinas@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/ajsalinas2/tercerRepo",     # URL del proyecto
+)
